c5217ef9d16d9da703e99a3d878cdc402c3b277e
Jeff Spaulding <jeffery.spaulding@maine.edu>
2015-06-08 12:39:14 -0700

    Uploading the original source code to GitHub.

---
+++ data/js/create-case.js
@@ -0,0 +1,19 @@
+$('body > table:eq(3) > tbody:eq(0) > tr:eq(0) > td:eq(2)').prepend('<div id="buttonBar"></div>');
+
+$('#buttonBar').append('<div class="caseButton" id="createCase">Quick Case</div>');
+$('body').append('<div class="absoluteCenter" id="formPopup"></div>');
+
+$('#formPopup').hide();
+
+$('#createCase').button();
+
+$('#createCase').on('click', function(){
+	$('#formPopup').show();
+	var formUrl = chrome.extension.getURL('data/html/password-form.html');
+	$('#formPopup').load(formUrl);
+});
+
+var userId = "";
+var scriptTag = $('body > div:eq(0) > script:eq(0)').html();
+var match = /genappend_submiturl/.exec(scriptTag);
+userId = scriptTag.substring(match.index + 35, match.index + 39);
