63d8d2ca5435c216401bb298a576b13442eeebb0
Jeff Spaulding <jeffery.spaulding@maine.edu>
2015-06-08 17:02:39 -0700

    Added several comments to the Quick Case functionality.

--- data/js/mod-main.js
+++ data/js/mod-main.js
@@ -1,15 +1,21 @@
+// Grab the specific location within the HTML on the techsupport.csun.edu/ozSptMain.jsp page to insert the "Quick Case" button
 $('body > table:eq(3) > tbody:eq(0) > tr:eq(0) > td:eq(2)').prepend('<div id="buttonBar"></div>');
 
+// Insert the "Quick Case" <div> and give it button functionality
 $('#buttonBar').append('<div class="caseButton" id="createCase">Quick Case</div>');
-$('body').append('<div class="absoluteCenter" id="formPopup"></div>');
-
-$('#formPopup').hide();
-
 $('#createCase').button();
 
-$('#createCase').on('click', function(){
-	var formUrl = chrome.extension.getURL('data/html/quick-case-form.html');
+// Add a hidden <div> that will act as the form popup
+$('body').append('<div class="absoluteCenter" id="formPopup"></div>');
 
+// Hide the form popup by default
+$('#formPopup').hide();
+
+// When the "Quick Case" button is clicked, display and load the Quick Case form
+$('#createCase').on('click', function(){
+	// Grab the extension-specific link to the Quick Case form HTML
+	var formUrl = chrome.extension.getURL('data/html/quick-case-form.html');
+	// Show the form popup and load the Quick Case form HTML
 	$('#formPopup').show();
 	$('#formPopup').load(formUrl);
 });
--- data/js/mod-summary.js
+++ data/js/mod-summary.js
@@ -1,15 +1,22 @@
+// Grab the specific location within the HTML on the techsupport.csun.edu/ozSptCaseSumry.jsp page to insert the "Quick Case" button
 $('body > table:eq(3) > tbody:eq(0) > tr:eq(0) > td:eq(3)').prepend('<div style="margin-bottom:0px;" id="buttonBar"></div>');
 
+// Insert the "Quick Case" <div> and give it button functionality
 $('#buttonBar').append('<div class="caseButton" id="createCase">Quick Case</div>');
-$('body').append('<div class="absoluteCenter" id="formPopup"></div>');
-
-$('#formPopup').hide();
-
 $('#createCase').button();
 
+// Add a hidden <div> that will act as the form popup
+$('body').append('<div class="absoluteCenter" id="formPopup"></div>');
+
+// Hide the form popup by default
+$('#formPopup').hide();
+
+// When the "Quick Case" button is clicked, display and load the Quick Case form
 $('#createCase').on('click', function(){
-	$('#formPopup').show();
+	// Grab the extension-specific link to the Quick Case form HTML
 	var formUrl = chrome.extension.getURL('data/html/quick-case-form.html');
+	// Show the form popup and load the Quick Case form HTML
+	$('#formPopup').show();
 	$('#formPopup').load(formUrl);
 });
 
